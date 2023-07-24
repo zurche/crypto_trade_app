404154d9345692da6172a3fe41005fbf0d7ab4ef
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2023-07-24 11:51:48 +0100

    Adding vertical gradient to background

--- app/src/main/java/com/az/cryptotradeapp/ui/composables/CTAMainScreen.kt
+++ app/src/main/java/com/az/cryptotradeapp/ui/composables/CTAMainScreen.kt
@@ -1,6 +1,8 @@
 package com.az.cryptotradeapp.ui.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,6 +12,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,17 +27,36 @@ fun CTAMainScreen() {
     Scaffold(bottomBar = {
         CTABottomNavigationBar()
     }, content = { paddingValues ->
-        Card(
+        CTAHomeContent(paddingValues)
+    })
+}
+
+@Composable
+@Preview(device = Devices.PIXEL_4, showBackground = true)
+private fun CTAHomeContent(paddingValues: PaddingValues = PaddingValues(0.dp)) {
+    val verticalWhiteGradient = Brush.verticalGradient(
+        colors = listOf(
+            FullWhite,
+            PaleWhite
+        )
+    )
+
+    Card(
+        modifier = Modifier
+            .background(color = FullWhite)
+            .fillMaxSize()
+            .padding(paddingValues),
+        colors = CardDefaults.cardColors(
+            containerColor = PaleWhite
+        ),
+        shape = RoundedCornerShape(30.dp)
+    ) {
+        Box(
             modifier = Modifier
-                .background(color = FullWhite)
                 .fillMaxSize()
-                .padding(paddingValues),
-            colors = CardDefaults.cardColors(
-                containerColor = PaleWhite
-            ),
-            shape = RoundedCornerShape(30.dp)
+                .background(brush = verticalWhiteGradient)
         ) {
 
         }
-    })
+    }
 }
