334c90672a3bc77c6b067abaabd72a771130c455
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2023-07-27 14:34:33 +0100

    Updating Bar Chart implementation

--- app/src/main/java/com/az/cryptotradeapp/ui/composables/home/CTAMyCryptoCap.kt
+++ app/src/main/java/com/az/cryptotradeapp/ui/composables/home/CTAMyCryptoCap.kt
@@ -11,14 +11,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -26,6 +24,7 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -94,6 +93,7 @@ fun CTAMyCryptoCap(modifier: Modifier = Modifier, data: MyCryptoCapUIData = mock
                 )
 
                 Text(
+                    modifier = Modifier.padding(bottom = 25.dp),
                     text = "${data.value} ${data.currency}",
                     color = Color.White,
                     style = MaterialTheme.typography.displayMedium,
@@ -109,20 +109,32 @@ fun CTAMyCryptoCap(modifier: Modifier = Modifier, data: MyCryptoCapUIData = mock
 
 @Composable
 fun MonthlyCapPreview(monthlyPreview: List<Pair<String, Float>>) {
-    val maxMonthValue = monthlyPreview.maxBy { it.second }.second
-    Row(
-        modifier = Modifier.fillMaxSize(),
-        verticalAlignment = Alignment.Bottom
-    ) {
-        for (pairPreview in monthlyPreview) {
-            val columnHeightWeight = pairPreview.second / maxMonthValue
-            Card(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight(columnHeightWeight)
-                    .padding(5.dp)
-            ) {
-                Text(text = pairPreview.first)
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        val maxMonthValue = monthlyPreview.maxBy { it.second }.second
+        Row(
+            modifier = Modifier.fillMaxHeight(.8f),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            for (pairPreview in monthlyPreview) {
+                val columnHeightWeight = pairPreview.second / maxMonthValue
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(columnHeightWeight)
+                        .padding(5.dp)
+                ) { }
+            }
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            for (pairPreview in monthlyPreview) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = pairPreview.first,
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
